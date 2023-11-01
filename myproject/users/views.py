b4b6688e5aed1c39e70659d07b76cb2351c74793
TTejaswanth <tejaswanth.t20@iiits.in>
2023-11-01 20:12:41 +0530

    signup

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -12,22 +12,23 @@ def user_list(request):
 def signup(request):
     if request.user.is_authenticated:
         return redirect('/')
-    
+
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
+            username = request.POST['username']
+            password = request.POST['password1']
 
-            user = form.save(commit=True)  # Create a user instance but don't save it yet
-            user.set_password(password)  # Set the password
-            user.save()  # Save the user
+            user = CustomUser(username=username,password=password)  # Create a user instance
+            # user.set_password(password)  # Set the password
+            user.save()  # Save the user to the database
 
             user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)  # Log in the user
-                messages.success(request, 'You have successfully logged in.')
-                return redirect('login')  # Redirect to the login page
+            return redirect('login')
+            # if user is not None:
+            #     login(request, user)  # Log in the user
+            #     messages.success(request, 'You have successfully logged in.')
+            #     return redirect('login')  # Redirect to the login page
         else:
             return render(request, 'signup.html', {'form': form, 'error_message': 'User not enrolled. Please try again.'})
     else:
@@ -36,23 +37,26 @@ def signup(request):
 
 def user_login(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
+        username = request.POST['username']
+        password = request.POST['password']
 
-            if user is not None:
-                login(request, user)
-                return redirect('profile')
-            else:
-                # Authentication failed, so display an error message
-                form.add_error(None, 'Invalid username or password. Please try again.')
+        user = authenticate(request, username=username, password=password)
 
-    else:
-        form = AuthenticationForm()
-    
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have successfully logged in.')
+            return redirect('profile')
+        else:
+            # Authentication failed, so display an error message
+            messages.error(request, 'Invalid username or password. Please try again.')
+
+    form = {}  # You can remove the form variable if you're not using it
+
     return render(request, 'login.html', {'form': form})
 
+
+# from your_app.models import CustomUser
+# user = CustomUser(username='testuser', password='password')
+# user.save()
 
 
